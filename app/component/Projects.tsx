2ba520cf00722ff4289ab2c297b26ca0609c1a6a
Tejas Banait <tejas@example.com>
2025-06-20 17:42:29 +0530

    Update Project

--- app/component/Projects.tsx
+++ app/component/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SiFirebase, SiMongodb, SiNextdotjs, SiNodedotjs, SiReact, SiRedux, SiSocketdotio, SiTailwindcss, SiTypescript } from 'react-icons/si'
+import { SiFirebase, SiMongodb, SiNextdotjs, SiNodedotjs, SiOpenai, SiReact, SiRedux, SiShadcnui, SiTailwindcss, SiTypescript } from 'react-icons/si'
 import Title from './Title'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
@@ -7,32 +7,32 @@ import { DirectionAwareHover } from '@/components/ui/direction-aware-hover'
 
 export default function Projects() {
     const projects = [
-        {
-            title: 'Talko Real Time Chat Application',
-            tech: [SiReact, SiNodedotjs, SiMongodb, SiSocketdotio],
-            link: 'https://talko-6y6r.onrender.com/',
-            cover: '/Talko.png',
+         {
+            title: 'Book Review Platform',
+            tech: [SiReact, SiNodedotjs, SiMongodb, SiTailwindcss],
+            link: 'https://book-review-platform-fawn.vercel.app/',
+            cover: '/bookreview.png',
             background: "bg-green-500"
         },
         {
             title: 'Shoppi Ecommerce Platform',
             tech: [SiReact, SiTailwindcss, SiRedux, SiFirebase],
             link: 'https://shoppi-e-commerce.vercel.app/',
-            cover: '/shop.png',
+            cover: '/shop1.png',
             background: "bg-indigo-500"
         },
         {
-            title: 'InstantEat food deliver',
-            tech: [SiReact, SiTailwindcss, SiRedux, SiFirebase],
-            link: 'https://github.com/Tejas-b463/FoodDelivery',
-            cover: '/fooddeliver.png',
+            title: 'SoftSell',
+            tech: [SiNextdotjs, SiTailwindcss, SiShadcnui],
+            link: 'https://soft-sell-rho.vercel.app',
+            cover: '/softsell.png',
             background: "bg-cyan-500"
         },
         {
-            title: 'Portfolio',
-            tech: [SiNextdotjs, SiTypescript, SiTailwindcss],
-            link: '',
-            cover: '/project.png',
+            title: 'Interview AI Coach',
+            tech: [SiNextdotjs, SiTypescript, SiShadcnui, SiOpenai],
+            link: 'https://catalyst-rust.vercel.app',
+            cover: '/interviewcoach.png',
             background: "bg-fuchsia-500"
         }
     ]
